49f39cf9c4a1b72cb9bba5120e76b989ca921ffe
Mac-Adam <80308649+Mac-Adam@users.noreply.github.com>
2024-08-20 22:35:22 +0200

    selective bloom

--- website/src/Bundle.js
+++ website/src/Bundle.js
@@ -2,6 +2,7 @@ import * as THREE from "three";
 import { LineMaterial } from "three/addons/lines/LineMaterial.js";
 import { Wireframe } from "three/addons/lines/Wireframe.js";
 import { WireframeGeometry2 } from "three/addons/lines/WireframeGeometry2.js";
+import { BLOOM_SCENE } from "./Common";
 
 class Bundle {
   constructor(size, position, color, scene) {
@@ -11,31 +12,27 @@ class Bundle {
 
     let geo = new THREE.IcosahedronGeometry(size, 1);
     const geometry_w = new WireframeGeometry2(geo);
-
     const matLine = new LineMaterial({
       color: color,
       linewidth: 5, // in pixels
       dashed: false,
     });
-
     const wireframe = new Wireframe(geometry_w, matLine);
     wireframe.computeLineDistances();
-    wireframe.scale.set(1, 1, 1);
     scene.add(wireframe);
     wireframe.position.set(...position);
-
+    wireframe.layers.enable(BLOOM_SCENE);
     this.edge_mesh = wireframe;
 
     const geometry = new THREE.BoxGeometry(size, size, size);
     const material = new THREE.MeshPhongMaterial({ color: 0xffffff });
     const inner_mesh = new THREE.Mesh(geometry, material);
     scene.add(inner_mesh);
-    inner_mesh.castShadow = true;
-    inner_mesh.receiveShadow = true;
     inner_mesh.position.set(...position);
     this.inner_mesh = inner_mesh;
   }
   animate() {
+    //During firs frame it is sometimes not yet set up, not sure why
     if (!this.edge_mesh) {
       return;
     }
--- website/src/Three.js
+++ website/src/Three.js
@@ -6,66 +6,133 @@ import { EffectComposer } from "three/addons/postprocessing/EffectComposer.js";
 import { RenderPass } from "three/addons/postprocessing/RenderPass.js";
 import { UnrealBloomPass } from "three/addons/postprocessing/UnrealBloomPass.js";
 import { OutputPass } from "three/addons/postprocessing/OutputPass.js";
+import { ShaderPass } from "three/addons/postprocessing/ShaderPass.js";
+import { BLOOM_SCENE, vertexShader, fragmentShader } from "./Common";
 function MyThree() {
   const refContainer = useRef(null);
   const refBundles = useRef([]);
+  const refMaterials = useRef({});
+
   useEffect(() => {
-    // === THREE.JS CODE START ===
+    const darkMaterial = new THREE.MeshBasicMaterial({ color: "black" });
+    const bloomLayer = new THREE.Layers();
+
+    //functions used for selective bloom,
+    //I am not 100% how it works, But i think It swaps the material for not bloomed objects for a pure black, which makes them invisible
+    // Then returns the materials to normal for the normal render.
+    function darkenNonBloomed(obj) {
+      if (obj.isMesh && bloomLayer.test(obj.layers) === false) {
+        refMaterials.current[obj.uuid] = obj.material;
+        obj.material = darkMaterial;
+      }
+    }
+
+    function restoreMaterial(obj) {
+      if (refMaterials.current[obj.uuid]) {
+        obj.material = refMaterials.current[obj.uuid];
+        delete refMaterials.current[obj.uuid];
+      }
+    }
+
+    //Setup render
+    bloomLayer.set(BLOOM_SCENE);
+
     var scene = new THREE.Scene();
     var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
     var renderer = new THREE.WebGLRenderer({ antialias: true });
 
-    const composer = new EffectComposer(renderer);
-    composer.addPass(new RenderPass(scene, camera));
+    const renderScene = new RenderPass(scene, camera);
     const bloomPass = new UnrealBloomPass(
       new THREE.Vector2(window.innerWidth, window.innerHeight),
-      1, // strength
+      0.3, // strength
       0.1, // radius
-      0.4 // threshold
+      0.1 // threshold
     );
-    composer.addPass(bloomPass);
-    const outputPass = new OutputPass();
-    composer.addPass(outputPass);
+    const bloomComposer = new EffectComposer(renderer);
+    bloomComposer.renderToScreen = false;
+    bloomComposer.addPass(renderScene);
+    bloomComposer.addPass(bloomPass);
+    bloomComposer.setSize(window.innerWidth, window.innerHeight);
 
-    renderer.shadowMap.enabled = true;
+    const mixPass = new ShaderPass(
+      new THREE.ShaderMaterial({
+        uniforms: {
+          baseTexture: { value: null },
+          bloomTexture: { value: bloomComposer.renderTarget2.texture },
+        },
+        vertexShader: vertexShader,
+        fragmentShader: fragmentShader,
+        defines: {},
+      }),
+      "baseTexture"
+    );
+    mixPass.needsSwap = true;
+
+    const outputPass = new OutputPass();
+
+    const finalComposer = new EffectComposer(renderer);
+    finalComposer.addPass(renderScene);
+    finalComposer.addPass(mixPass);
+    finalComposer.addPass(outputPass);
+    finalComposer.setSize(window.innerWidth, window.innerHeight);
+
     renderer.setSize(window.innerWidth, window.innerHeight);
-    composer.setSize(window.innerWidth, window.innerHeight);
     const controls = new OrbitControls(camera, renderer.domElement);
     camera.position.z = 5;
     controls.target.set(0, 0, 0);
     controls.update();
 
-    const color = 0xfaf5de;
+    const render = () => {
+      //render bloom
+      scene.traverse(darkenNonBloomed);
+      bloomComposer.render();
+      scene.traverse(restoreMaterial);
+
+      // render the entire scene, then render bloom scene on top
+      finalComposer.render();
+    };
+    window.onresize = () => {
+      const width = window.innerWidth;
+      const height = window.innerHeight;
+
+      camera.aspect = width / height;
+      camera.updateProjectionMatrix();
+
+      renderer.setSize(width, height);
+
+      bloomComposer.setSize(width, height);
+      finalComposer.setSize(width, height);
+
+      render();
+    };
+
+    //setupLights
+    const color = 0xfae5ce;
     const intensity = 1.3;
     const light = new THREE.DirectionalLight(color, intensity);
-    light.castShadow = true;
     light.position.set(0, 10, 0);
     light.target.position.set(-5, 0, 0);
     scene.add(light);
     scene.add(light.target);
 
-    const color2 = 0xfae8de;
+    const color2 = 0xfad8ce;
     const intensity2 = 0.8;
     const light2 = new THREE.DirectionalLight(color2, intensity2);
-    light2.castShadow = true;
     light2.position.set(-10, 3, 0);
     light2.target.position.set(0, 0, 5);
     scene.add(light2);
     scene.add(light2.target);
 
-    const color3 = 0xdeeafa;
+    const color3 = 0xcedafa;
     const intensity3 = 2;
     const light3 = new THREE.DirectionalLight(color3, intensity3);
-    light3.castShadow = true;
     light3.position.set(3, -10, 30);
     light3.target.position.set(0, 10, -5);
     scene.add(light3);
     scene.add(light3.target);
 
-    //document.body.appendChild(renderer.domElement);
-    // use ref as a mount point of the Three.js scene instead of the document.body
     refContainer.current && refContainer.current.appendChild(renderer.domElement);
-
+    // Create bundles
     refBundles.current.push(new Bundle(1.5, [2, 0, 0], 0x00ff00, scene));
     refBundles.current.push(new Bundle(0.5, [-2, 0, 0], 0xffff00, scene));
     refBundles.current.push(new Bundle(1, [2, 4, 0], 0x00ffff, scene));
@@ -74,10 +141,11 @@ function MyThree() {
     var animate = function () {
       requestAnimationFrame(animate);
       refBundles.current.forEach((b) => b.animate(0.01, 0.01, 0.01));
-      composer.render(scene, camera);
+      render();
     };
     animate();
   }, []);
+
   return <div ref={refContainer}></div>;
 }
 
