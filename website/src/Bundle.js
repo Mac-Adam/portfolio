49efb727400569390301733f1d93259d2e6c8e20
Mac-Adam <80308649+Mac-Adam@users.noreply.github.com>
2024-08-20 15:28:04 +0200

    No need for custom ico_sphere model

--- website/src/Bundle.js
+++ website/src/Bundle.js
@@ -1,29 +1,30 @@
 import * as THREE from "three";
-import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+import { LineMaterial } from "three/addons/lines/LineMaterial.js";
+import { Wireframe } from "three/addons/lines/Wireframe.js";
+import { WireframeGeometry2 } from "three/addons/lines/WireframeGeometry2.js";
 
 class Bundle {
   constructor(size, position, color, scene) {
     this.rotation_anim_x = Math.random() * 0.01 - 0.005;
     this.rotation_anim_y = Math.random() * 0.01 - 0.005;
     this.rotation_anim_z = Math.random() * 0.01 - 0.005;
-    const loader = new GLTFLoader();
-    loader.load(
-      "/models/ico_sphere.glb",
-      (gltf) => {
-        const model = gltf.scene;
-        const edge_mesh = model.children[0]; // make sure the glb file has just one child, the model.
-        edge_mesh.material = new THREE.MeshBasicMaterial({ color: color, emissive: color, emissiveIntensity: 2 });
-        edge_mesh.material.wireframe = true;
-        scene.add(edge_mesh);
-        edge_mesh.position.set(...position);
-        edge_mesh.scale.set(size, size, size);
-        this.edge_mesh = edge_mesh;
-      },
-      undefined,
-      function (error) {
-        console.error(error);
-      }
-    );
+
+    let geo = new THREE.IcosahedronGeometry(size, 1);
+    const geometry_w = new WireframeGeometry2(geo);
+
+    const matLine = new LineMaterial({
+      color: color,
+      linewidth: 5, // in pixels
+      dashed: false,
+    });
+
+    const wireframe = new Wireframe(geometry_w, matLine);
+    wireframe.computeLineDistances();
+    wireframe.scale.set(1, 1, 1);
+    scene.add(wireframe);
+    wireframe.position.set(...position);
+
+    this.edge_mesh = wireframe;
 
     const geometry = new THREE.BoxGeometry(size, size, size);
     const material = new THREE.MeshPhongMaterial({ color: 0xffffff });
